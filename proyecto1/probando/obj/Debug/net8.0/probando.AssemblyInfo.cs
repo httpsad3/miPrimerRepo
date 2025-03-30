04dc9558b9f01962751fa060f8ab475dbf2edfd3
anton!o <antonioastocondor@gmail.com>
2025-03-30 18:57:51 -0500

    nuevo gitignore creado

--- proyecto1/probando/obj/Debug/net8.0/probando.AssemblyInfo.cs
+++ proyecto1/probando/obj/Debug/net8.0/probando.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("probando")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+521ed761e72e32e61fdd1b93c2d5e6ae923e141e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2c50074a8a1bd01841ae9859eaa782f9f875e76a")]
 [assembly: System.Reflection.AssemblyProductAttribute("probando")]
 [assembly: System.Reflection.AssemblyTitleAttribute("probando")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
